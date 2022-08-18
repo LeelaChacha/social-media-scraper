3244891eb9bfdd6c6b29fdbdbdfb652943067388
LeelaChacha <me@hukumraj-singh-deora.party>
2022-08-18 13:13:40 +0200

    Initial Commit

---
+++ src/main/java/party/hukumraj_singh_deora/App.java
@@ -0,0 +1,9 @@
+package party.hukumraj_singh_deora;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/party/hukumraj_singh_deora/AppTest.java
@@ -0,0 +1,14 @@
+package party.hukumraj_singh_deora;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
